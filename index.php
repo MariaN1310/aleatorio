b9fb3cb363b568ea27c3556c16bec621479dad99
Mariano Blanco <blanco.mariano13@gmail.com>
2024-01-27 13:16:02 +0100

    Idea inicial.

--- index.php
+++ index.php
@@ -1,5 +1,13 @@
-<?php
+Permitir pegar en un textbox texto vario
+separado entre saltos de lineas, comas o guiones.
 
-echo "Hola primer git";
-$hola = "hola";
-?>
+Maximo 1000 caracteres en un principio.
+
+Seleccionar tipo de separación y seleccionar 1,2,3,4 o 5 de forma aleatoria.
+
+No necesita DB. Usar bootstrap ultimo y js ultimo.
+Intentar hacer todo con JS y no con PHP.
+
+Poner seguridad, evitar caracteres raros.
+
+En un futuro, ver si se copia texto de "comentarios de instagram", que lo separe y pegue solo los nombres o algo asi.
